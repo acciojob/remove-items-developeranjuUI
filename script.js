be2de5cda9287b93e397826b4ee0142d989e5b7a
developeranjuUI <116105555+developeranjuUI@users.noreply.github.com>
2025-03-31 16:19:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 //your JS code here. If required.
 function removeColor(){
 	let selectElement = document.getElementById("colorSelect");
-	let selectoptions = selectElement.options[selectElement.selectIndex];
-	if(selectoptions){
-		selectoptions.remove();
+	if(selectElement.selectIndex !== -1){
+		selectElement.remove(selectElement.selectIndex);
 	}
 }
 document.getElementById('colorSelect').addEventListener('click',removeColor);
